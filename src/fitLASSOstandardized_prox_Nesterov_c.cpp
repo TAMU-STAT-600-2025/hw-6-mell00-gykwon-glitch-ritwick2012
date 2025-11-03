f77722e1e8b33d03f914f080e0df287705d8c5f1
Madison Ell <mellmell2550@gmail.com>
2025-11-02 22:45:25 -0600

    Add main Nesterov implementation loop to Nesterov LASSO C++ function

--- src/fitLASSOstandardized_prox_Nesterov_c.cpp
+++ src/fitLASSOstandardized_prox_Nesterov_c.cpp
@@ -64,6 +64,66 @@ arma::colvec fitLASSOstandardized_prox_Nesterov_c(const arma::mat& Xtilde, const
   
   double f_curr = f_eval(x_curr);
   double f_next = f_curr;
+  
+  
+  // Main loop
+  for (int iter = 0; iter < max_iter; ++iter) {
+    // r = Y - X * y_curr
+    r = Ytilde - Xtilde * y_curr;
+    
+    // grad = -(1/n) X^T r
+    for (int j = 0; j < p; ++j) grad.at(j) = 0.0;
+    for (int j = 0; j < p; ++j){
+      double acc = 0.0;
+      for (int i = 0; i < n; ++i) acc += Xtilde.at(i,j) * r.at(i);
+      grad.at(j) = -acc / n;
+    }
+    
+    // z = y_curr - s * grad
+    for (int j = 0; j < p; ++j) z.at(j) = y_curr.at(j) - s * grad.at(j);
+    
+    // x_next = soft(z, lambda*s)
+    arma::colvec x_next(p);
+    for (int j = 0; j < p; ++j) x_next.at(j) = soft_c(z.at(j), lambda * s);
+    
+    // Nesterov
+    double t_next = 0.5 * (1.0 + std::sqrt(1.0 + 4.0 * t_prev * t_prev));
+    double mom = (t_prev - 1.0) / t_next;
+    arma::colvec y_next(p);
+    for (int j = 0; j < p; ++j)
+      y_next.at(j) = x_next.at(j) + mom * (x_next.at(j) - x_curr.at(j));
+    
+    // monotone restart
+    f_next = f_eval(x_next);
+    if (!std::isfinite(f_next) || f_next > f_curr){
+      for (int j = 0; j < p; ++j) y_next.at(j) = x_next.at(j);
+      t_next = 1.0;
+      f_next = f_eval(x_next);
+    }
+    
+    // convergence: relative L1
+    double num = 0.0, den = 1.0;
+    for (int j = 0; j < p; ++j){
+      double diff = x_next.at(j) - x_curr.at(j);
+      num += std::sqrt(diff * diff);
+      den += std::sqrt(x_curr.at(j) * x_curr.at(j));
+    }
+    if (num / den < eps){
+      for (int j = 0; j < p; ++j) beta.at(j) = x_next.at(j);
+      return beta;
+    }
+    
+    // update (element-wise)
+    for (int j = 0; j < p; ++j) x_curr.at(j) = x_next.at(j);
+    for (int j = 0; j < p; ++j) y_curr.at(j) = y_next.at(j);
+    f_curr = f_next;
+    t_prev = t_next;
+    
+    if (iter == max_iter - 1)
+      for (int j = 0; j < p; ++j) beta.at(j) = x_curr.at(j);
+  }
+  
+  
 
   return beta;
 }
